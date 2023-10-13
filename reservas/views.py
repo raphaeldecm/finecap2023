c55d23ffd3c11e663518667bc94aabfc6f6399f1
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2023-10-13 17:13:31 -0300

    gerente permissions to reservas views

--- reservas/views.py
+++ reservas/views.py
@@ -1,32 +1,33 @@
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages import views
 from django.urls import reverse_lazy
 from django.views import generic
+from users.permissions import GerentePermission
 
 from .forms import ReservaForm
 from .models import Reserva
 
 
-class ReservasListView(LoginRequiredMixin, generic.ListView):
+class ReservasListView(GerentePermission, LoginRequiredMixin, generic.ListView):
     model = Reserva
     paginate_by = 5
 
-class ReservaDetailView(LoginRequiredMixin, generic.DetailView):
+
+class ReservaDetailView(GerentePermission, LoginRequiredMixin, generic.DetailView):
     model = Reserva
 
-class ReservaCreateView(LoginRequiredMixin, views.SuccessMessageMixin, generic.CreateView):
+class ReservaCreateView(GerentePermission, LoginRequiredMixin, views.SuccessMessageMixin, generic.CreateView):
     model = Reserva
     form_class = ReservaForm
     success_url = reverse_lazy("reservas:reservas-list")
     success_message = "Reserva cadastrada com sucesso!"
 
-class ReservaUpdateView(LoginRequiredMixin, views.SuccessMessageMixin, generic.UpdateView):
+class ReservaUpdateView(GerentePermission, LoginRequiredMixin, views.SuccessMessageMixin, generic.UpdateView):
     model = Reserva
     form_class = ReservaForm
     success_url = reverse_lazy("reservas:reservas-list")
     success_message = "Reserva atualizada com sucesso!"
 
-class ReservaDeleteView(LoginRequiredMixin, generic.DeleteView):
+class ReservaDeleteView(GerentePermission, LoginRequiredMixin, generic.DeleteView):
     model = Reserva
     success_url = reverse_lazy("reservas:reservas-list")
