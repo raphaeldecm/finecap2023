7c1292f596fee882cbacf8452d0b88b283b19581
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2023-10-26 11:16:52 -0300

    set created and updated by

--- stands/views.py
+++ stands/views.py
@@ -29,6 +29,10 @@ class StandCreateView(LoginRequiredMixin, views.SuccessMessageMixin, generic.Cre
     success_url = reverse_lazy("stands:stands-list")
     success_message = "Stand cadastrado com sucesso!"
 
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        form.instance.updated_by = self.request.user
+        return super().form_valid(form)
 
 class StandUpdateView(LoginRequiredMixin, views.SuccessMessageMixin, generic.UpdateView):
     model = Stand
